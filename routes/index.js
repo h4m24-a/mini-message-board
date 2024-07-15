5a1d3b2e1902a87154960ee9c97e7ff214342313
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-07-15 21:25:49 +0100

    set up routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express'})
+  res.render('index', { title: 'Message Board'})
+})
+
+router.get('/new', (req,res) => {
+  res.send('from new url')
 })
 
 module.exports = router;
