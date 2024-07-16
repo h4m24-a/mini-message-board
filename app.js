e7323436819fd3f0322784c88d8dae97ee1cd9f3
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-07-16 17:32:27 +0100

    added template page for creating a new message and setting up post request to submit a new message

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ let indexRouter = require('./routes/index')
 const app = express();
 
 
+// Body parser middleware
+app.use(express.json());  // submit raw json
+app.use(express.urlencoded({ extended: false }));  // takes in an object - replicates web form and sends form data.
+
+// serve static files
+app.use(express.static('public'));
+
 
 //  There are two parts to setting up the engine. First, we set the 'views' value to specify the folder where the templates will be stored (in this case the subfolder /views). 
 //  Then we set the 'view engine' value to specify the template library (in this case "ejs").
@@ -15,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-// We add our route-handling code to the request handling chain. This will define particular routes for the different parts of the site
+// Adding route-handling code to the request handling chain. This will define particular routes for the different parts of the site
 app.use('/', indexRouter);
 
 
--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,35 @@ const messages = [
 ]
 
 router.get('/', (req, res) => {
-  res.render('index', { title: "Mini Messageboard", messages: messages })
+  res.render('index', { title: "Mini Messageboard", messagesA: messages }) // messagesA (red) is a key in the object
 })
 
 router.get('/new', (req,res) => {
-  res.send('from new url')
+  res.render('form',)
+})
+
+router.post('/new', (req,res) => {
+  const messageUser = req.body.messageUser;
+  const messageText = req.body.messageText;
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  res.redirect('/')
+  console.log('POST Request')
 })
 
 module.exports = router;
+
+
+
+
+/*
+Line 19:
+ This is an object containing key-value pairs that are passed to the view template. 
+ These pairs become variables that can be used within the view template.
+
+
+messagesA(red): messages sets a variable messagesA(red) in the view to the value of the messages variable from the current scope (array of objects).
+
+
+Therefore, messages(red) in this context is an array of objects that is being passed to the view template named 'index'. 
+This array can then be used within the view to display the messages on the web page.
+*/
