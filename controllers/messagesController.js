da98382373974de53c9524cc709fe511ad36b657
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-09-01 23:33:01 +0100

    updated controller

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,20 +1,20 @@
 const db = require('../db/queries');
 
 async function getMessages(req, res) {
-  const messages =  await db.getAllMessages();
+  const messages =  await db.getAllMessages();  // displays all messages using the db query
   res.render('index', { 
     title: "Mini Message Board", 
-    messagesA: messages // messagesA  is a key in the object
+    messagesA: messages // messagesA  is a key in the object, allows us to use this variable in index.ejs to display each message.
   }); 
 }
 
 
 async function createMessageGet(req, res) {
-  res.render('form',)
+  res.render('form',)   // renders  form.ejs
 }
 
 
-async function createMessagePost(req, res) {
+async function createMessagePost(req, res) {   
   const messageUser = req.body.messageUser;  // user: This property is set to the value of the variable messageUser, which contains the identifier of the user who sent the message.
   const messageText = req.body.messageText; // text: This property is set to the value of the variable messageText, which contains the content of the message.
   await db.insertMessage(messageUser, messageText)
@@ -28,3 +28,12 @@ module.exports = {
   createMessageGet,
   createMessagePost
 }
+
+
+/*
+messageUser is the name attribute of the input form for username
+messageUser is the name attribute of the input form for text.
+
+Using their attribute allows us to retrieve the value when the post request is made.
+
+*/
