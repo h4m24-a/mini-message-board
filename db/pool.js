4805d01d75189599c09a561af916e3aa1633692b
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-09-06 19:51:24 +0100

    hosting

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === 'production';
+
 
 module.exports = new Pool({
   host: process.env.HOST,
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.PORT, //  default is 5432
-  ssl: isProduction ? { rejectUnauthorized: false } : false, // Enable SSL in production if required
+  port: process.env.PORT,
+  url: process.env.DEPLOYED_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
