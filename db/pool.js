279bbfe9ed249a577c1b824c3c62a8dd5cc5ee4b
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-10-07 16:38:56 +0100

    db

--- db/pool.js
+++ db/pool.js
@@ -3,13 +3,5 @@ const { Pool } = require('pg');
 
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-  internal_url: process.env.INTERNAL_URL, 
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // }
+  database_url: process.env.DATABASE_URL
 });
